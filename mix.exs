de0d8a6d8a58882fb32f56ebf034308081fba554
5HT <maxim@synrc.com>
2023-03-24 04:36:45 +0200

    fix retired

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sample.Mixfile do
   def project() do
     [
       app: :sample,
-      version: "4.11.1",
+      version: "6.6.7",
       description: "SAMPLE Elixir Application",
       package: package(),
       elixir: "~> 1.7",
